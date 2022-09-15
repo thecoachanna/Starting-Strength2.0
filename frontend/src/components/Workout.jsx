7848740fe2a889ecc5882644c68f8f04cf4601a8
thecoachanna <nettles.anna@gmail.com>
2022-09-14 23:43:51 -0400

    card styling

--- frontend/src/components/Workout.jsx
+++ frontend/src/components/Workout.jsx
@@ -1,28 +1,47 @@
-import { Fragment } from 'react'
+import { Fragment } from "react";
+// import Card from "react-bootstrap/Card";
+// import ListGroup from "react-bootstrap/ListGroup";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css"
 
-const Workout = ({workouts}) => {
+
+const Workout = ({ workouts }) => {
   return (
-    <Fragment>      
-    {workouts.map((workout) => {
-      return (
-        <div key={workout.id}>
-              <h4>{workout?.number}</h4>
-              {workout.exercises.map((exercise) => {
-                  return (
+    <Fragment>
+      {workouts.map((workout) => {
+        return (
+          <div className="card" key={workout.id}>
+            <span className="title">{workout?.number}</span>
+            {workout.exercises.map((exercise) => {
+              return (
                 <div key={exercise.id}>
-                          <p>{exercise.exercise.name}</p>
-                          <iframe width="450" height="250" src={exercise.exercise.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                          <p>{exercise.exercise.cues}</p>
-                          <p>Sets: {exercise.sets}</p>
-                          <p>Reps: {exercise.reps}</p>
-                          </div>
-                  )
-              })}          
-        </div>
-      );
-    })}
-  </Fragment>
-  )
-}
+                  <iframe
+                    width="395"
+                    height="250"
+                    src={exercise.exercise.video}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                  
+                    <p className="title">{exercise.exercise.name}</p>
+                    <p className="desc">{exercise.exercise.cues}</p>
+                    <p className="desc">Sets: {exercise.sets}</p>
+                    <p className="desc">Reps: {exercise.reps}</p>
+                   
+                    <label className="switch">Check box to mark complete:                      
+                      <input type="checkbox" />
+                  </label>
+                  <br></br>                    
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </Fragment>
+  );
+};
 
-export default Workout
+export default Workout;
