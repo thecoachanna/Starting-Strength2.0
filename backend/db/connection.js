aef943458c747ed0604c6f88d53eb53b3bd9c2e1
thecoachanna <nettles.anna@gmail.com>
2022-09-19 15:52:41 -0400

    Deploy to Heroku

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoURI = 'mongodb://localhost:27017/starting-strength'
+const mongoURI = process.env.DATABASE_URL
     
 
 mongoose.connect(mongoURI, {
